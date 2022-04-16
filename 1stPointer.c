76ad63104eb239428bdfcdf68901afd2cacb45b0
Sanjh Raj <94918106+sanjh1234@users.noreply.github.com>
2022-04-16 15:38:40 +0530

    Update 1stPointer.c

--- 1stPointer.c
+++ 1stPointer.c
@@ -5,13 +5,24 @@ int main(){
     int *ptr = &age;
     int _age = *ptr;
 
+    //for printing value at age i.e. 18
+    printf("%d\n",age);
     printf("%d\n",*ptr);
     printf("%d\n",_age);
+    printf("%d\n",*(&age));
+    
+    //for address of age
     printf("%p\n",ptr);
     printf("%p\n",&age);
+    
+    //unsigned address of age
     printf("%u\n",ptr);
+    printf("%u\n",&age);
+    
+    //address of pointer
+    printf("%p\n",&ptr);
     printf("%u\n",&ptr);
-    printf("%d\n",*(&age));
+    
     
     return 0;
 }
