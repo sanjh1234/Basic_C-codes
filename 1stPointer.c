d5a8536eb8755609afed3cff8edbf1b429eb06c5
Sanjh Raj <94918106+sanjh1234@users.noreply.github.com>
2022-04-16 15:23:56 +0530

    Add files via upload

---
+++ 1stPointer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int age =18;
+    int *ptr = &age;
+    int _age = *ptr;
+
+    printf("%d\n",*ptr);
+    printf("%d\n",_age);
+    printf("%p\n",ptr);
+    printf("%p\n",&age);
+    printf("%u\n",ptr);
+    printf("%u\n",&ptr);
+}
